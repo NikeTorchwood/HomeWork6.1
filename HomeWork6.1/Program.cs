6aa49d3ed8168423fd1752a9c02f4683c1fd8c12
Nikolay Skurkov <ntwdconst@gmail.com>
2023-01-31 22:09:19 +0700

    its ready

--- HomeWork6.1/Program.cs
+++ HomeWork6.1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main()
+    {
+        var venus = new
+        {
+            Name = "Венера",
+            IdFromSun = 2,
+            EquatorLength = 38025,
+        };
+        var earth = new
+        {
+            Name = "Земля",
+            IdFromSun = 3,
+            EquatorLength = 40075,
+            PreviousPlanet = venus
+        };
+        var mars = new
+        {
+            Name = "Марс",
+            IdFromSun = 4,
+            EquatorLength = 21344,
+            PreviousPlanet = earth
+        };
+        var venus2 = new
+        {
+            Name = "Венера",
+            IdFromSun = 2,
+            EquatorLength = 38025,
+            PreviousPlanet = mars
+        };
+        PrintPlanet(venus,earth,mars,venus2);
+    }
+
+    private static void PrintPlanet(params object[] obj)
+    {
+        foreach (object item in obj)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
